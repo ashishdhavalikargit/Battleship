36e7b9560479f3bb34af10d3c1311638827c3805
Ashish Dhavalikar <ashish.dhavalikar@CALSOFTHQ.COM>
2019-06-06 18:39:59 +0530

    added sound effects

--- battleship.py
+++ battleship.py
@@ -1,4 +1,8 @@
 from arsenal import BattleShip
+from platform import python_version
+from sound_effects import playMP3
+
+take_input = input if int(python_version()[0]) == 3 else raw_input
 
 class Player(BattleShip):
 	pass
@@ -7,9 +11,10 @@ class Player(BattleShip):
 def main():
 	print("\n==================      welcome to battleship      ==================\n")
 	# include aound
+	playMP3("mp3tracks/ship_built.mp3")
 	status = True
 	while (status):
-		value = int(input("\nPlease enter ocean size (3 or more):  "))
+		value = int(take_input("\nPlease enter ocean size (3 or more):  "))
 		status = value < 3
 		if status:
 			print("\nOCEAN IS TOO SMALL EVEN FOR FISH TO SWIM \nPLEASE DREAM BIGGER ...")
@@ -24,15 +29,16 @@ def main():
 	print("\n ~ ~ ~ ~ Here is your wonderful ocean ~ ~ ~ ~ \n")
 	player1.show_ships()
 	while count:
-		ship_type = str(input("\nEnter ship type cargo-ship/submarine :  "))
-		start =  str(input("\nEnter ship initial point Ex.[A:3] :  "))
-		direction = str(input("\nEnter direction  right/left/up/down:  "))
+		ship_type = str(take_input("\nEnter ship type cargo-ship/submarine :  "))
+		start =  str(take_input("\nEnter ship initial point Ex.[A:3] :  "))
+		direction = str(take_input("\nEnter direction  right/left/up/down:  "))
 		status, msg = player1.create_ship(ship_type, start, direction)
 		if status:
 			count -= 1
 		print(msg)
 	player1.show_ships()
 	cpu.create_random_ships(ships)
-	# include sound
+	sound_effects.playMP3("D:/Battleship/Latest_built_06_06_2019/Battleship/mp3tracks/ship_built.mp3")	
+	playMP3("mp3tracks/ship_built.mp3")
 
 main()
---
+++ sound_effects.py
@@ -0,0 +1,14 @@
+from ctypes import *
+
+
+winmm = windll.winmm
+def mciSend(s):
+   i=winmm.mciSendStringA(s,0,0,0)
+   if i<0:
+      print "Error %d in mciSendString %s" % ( i, s )
+
+def playMP3(mp3Name):
+   mciSend("Close All")
+   mciSend("Open \"%s\" Type MPEGVideo Alias theMP3" % mp3Name)
+   mciSend("Play theMP3 Wait")
+   mciSend("Close theMP3")
